71f68f8b886f7f04fb16c71835123204e5cc7041
alfanaisy <alfanaisy7@gmail.com>
2024-07-04 03:28:35 +0700

    Setup basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+
 function App() {
-  return <div>Hello World</div>;
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { index: true, element: <h1>Home Page</h1> },
+        { path: '/my-list', element: <h1>My List Page</h1> },
+      ],
+    },
+    {
+      path: '/login',
+      element: <h1>Login Page</h1>,
+    },
+    {
+      path: '/register',
+      element: <h1>Register Page</h1>,
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
