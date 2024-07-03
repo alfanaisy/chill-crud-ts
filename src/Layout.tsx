7280622ef821bdadfdec2e440886c94ef81acddb
alfanaisy <alfanaisy7@gmail.com>
2024-07-04 05:29:54 +0700

    Build navbar component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/navbar/navbar.component';
 
+import styles from './index.module.css';
+
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className={styles.content}>
+        <Outlet />
+      </div>
     </>
   );
 };
--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,9 +1,39 @@
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'usehooks-ts';
+
+import Logo from '../../assets/logo.svg';
+import LogoSmall from '../../assets/logo-sm.svg';
+import Avatar from '../../assets/avatar.svg';
+
+import styles from './navbar.module.css';
+import { CgChevronDown } from 'react-icons/cg';
 
 const Navbar = () => {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
   return (
-    <nav className="navbar">
-      <Link to={'/'}></Link>
+    <nav className={styles.navbar}>
+      <div className={styles.navContainer}>
+        <Link to={'/'} className={styles.navLogo}>
+          <img src={isMobile ? LogoSmall : Logo} alt="navbar-logo" />
+        </Link>
+
+        <Link className={styles.navItem} to="/series">
+          <span>Series</span>
+        </Link>
+        <Link className={styles.navItem} to="/film">
+          <span>Film</span>
+        </Link>
+        <Link className={styles.navItem} to="/my-list">
+          <span>Daftar Saya</span>
+        </Link>
+      </div>
+      <div className={styles.profileMenu}>
+        <img src={Avatar} alt="avatar pic" />
+        <span>
+          <CgChevronDown />
+        </span>
+      </div>
     </nav>
   );
 };
